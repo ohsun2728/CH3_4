c727a02ce526139ce6da54acbb6aa07fcb2e91f2
ohsun2728 <ohsun2728@gmail.com>
2026-06-15 14:22:22 +0900

    FirstInit

---
+++ CH3_4/Source/CH3_4/CH3_4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CH3_4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH3_4, "CH3_4" );
